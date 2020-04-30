b07e830e9abbd7c13820f672fbcda8a70c08c7c7
kupaskulit <muhyib637@gmail.com>
2020-04-30 22:50:15 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+var data = [
+    
+];
+
+
+function tampil() {
+    var tabel = document.getElementById("tabel");
+    tabel.innerHTML = "<tr><th>No</th><th>Pelatihan</th></tr>";
+    for (let i = 0; i < data.length; i++) {
+        var btnEdit = "<button class='btn-edit' href='#' onclick='edit(" + i + ")'>Edit</button>";
+        var btnHapus = "<button class='btn-hapus' href='#' onclick='hapus(" + i + ")'>Hapus</button>";
+        j = i + 1;
+        tabel.innerHTML += "<tr><td>" + j + "</td><td>" + data[i] + "</td><td>" + btnEdit + " " + btnHapus + "</tr>";
+    }
+}
+
+function tambah() {
+    var input = document.querySelector("input[name=Pelatihan]");
+    data.push(input.value);
+    tampil();
+    input.value = "";
+}
+
+function edit(id) {
+    var baru = prompt("Edit", data[id]);
+    data[id] = baru;
+    tampil();
+}
+
+function hapus(id) {
+    data.pop(id);
+    tampil();
+}
+
+tampil();
